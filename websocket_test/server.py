27c6275112780bab92d33ca53aa9859bea803d2f
Jack Chou <jack.chou@medicustek.com>
2017-08-08 13:25:47 +0800

    Fix server side for testing.

--- websocket_test/server.py
+++ websocket_test/server.py
@@ -14,9 +14,9 @@ async def hello(websocket, path):
 sc = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
 # sc.check_hostname = False
 # sc.verify_mode = ssl.CERT_NONE
-sc.load_cert_chain('./server.pem', './server.key')
+sc.load_cert_chain('./resource/server.pem', './resource/server.key')
 
-start_server = websockets.serve(hello, 'localhost/123', 8765, ssl=sc)
+start_server = websockets.serve(hello, 'localhost', 8765, ssl=sc, subprotocols=[123, 32, "abc"])
 # start_server = websockets.serve(hello, 'localhost', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
