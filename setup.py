7c106ef406d2f8862af936246d42897b557aeea5
Jack Chou <jack.chou@medicustek.com>
2017-08-09 10:16:13 +0800

    0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wbt',
-    version='0.0.4',
+    version='0.0.5',
     description='A websocket client maker, used to test capability of channel and server.',
     license='MIT',
     author='Chou, Yi-Tang',
